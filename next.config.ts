eaece084de9b0c8b7261c793acf5a7ada0df78b1
Alan Barret <alanbarret98@gmail.com>
2025-10-26 13:39:54 +0000

    Still upload failed, could not upload image error

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
